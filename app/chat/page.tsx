2b4bb8d2e5f9000554f47b5771b777250b3395a0
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-05-09 23:34:40 +0530

    build chatpage(chatCamare)

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,7 +1,14 @@
+import ChatCamera from "@/components/ChatCamera";
 import React from "react";
 
 const ChatPage = () => {
-  return <div>chat page</div>;
+  return (
+    <div className="flex flex-grow items-center">
+      <div className="bg-chatImage bg-cover bg-right-bottom rounded-lg w-full h-[98%] flex m-2 items-center justify-center p-6">
+        <ChatCamera />
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
---
+++ components/ChatCamera.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { FaCamera } from "react-icons/fa";
+import React, { useRef, useState } from "react";
+import { readFileAsDataURL } from "@/lib/utils";
+
+const ChatCamera = () => {
+  const imageRef = useRef<HTMLInputElement>(null);
+  const [selectedFile, setSelectedFile] = useState<string>("");
+  const fileChangeHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target?.files?.[0];
+    if (file) {
+      const dataUrl = await readFileAsDataURL(file);
+      setSelectedFile(dataUrl);
+    }
+  };
+  console.log(selectedFile);
+
+  return (
+    <div className="flex flex-col items-center justify-center m-2 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5 border p-5">
+      <div
+        className="rounded-full p-8 bg-white-800 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-30 border border-gray-200 cursor-pointer text-white"
+        onClick={() => imageRef.current?.click()}
+      >
+        <FaCamera size={"30px"} />
+        <input
+          type="file"
+          accept="image/*"
+          hidden
+          ref={imageRef}
+          onChange={fileChangeHandler}
+        />
+      </div>
+      <p className="w-2/3 text-center text-white mt-4 font-semibold text-lg">
+        Let's send your fast snap.
+      </p>
+    </div>
+  );
+};
+
+export default ChatCamera;
